f928c8d98ac218253d7a380e6341ef10e414bc5d
Loctarogar <Morilon@protonmail.com>
2023-07-07 10:13:59 +0200

    blade layouts

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,20 +1,20 @@
-@extends('components.layout')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <!--    <script src="/app.js"></script>-->
+    <title>my blog</title>
+</head>
+<body>
+@yield('banner')
+@yield('content')
+</body>
+</html>
 
-@section('banner')
-    <h1>My Blog</h1>
-@endsection
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-            <div>{{ $post->excerpt }} </div>
-        </article>
-    @endforeach
-@endsection
 <!--{{--<!DOCTYPE html>--}}-->
 <!--{{--<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">--}}-->
 <!--{{--    <head>--}}-->
