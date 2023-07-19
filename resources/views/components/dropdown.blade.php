7756583af96da4bb5e124311ff0aa8539368b7dd
Loctarogar <Morilon@protonmail.com>
2023-07-19 20:30:55 +0200

    clean up in blade sheets

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     {{--  Links  --}}
-    <div x-show="show" class="">
+    <div x-show="show" class=" py-2 bg-gray-100 mt-2 rounded-xl w-full absolute z-50 overflow-auto max-h-52">
         {{ $slot }}
     </div>
 </div>
