47a2ef7364a0686ed6f733905ef1d9f0491b00a6
Loctarogar <Morilon@protonmail.com>
2023-07-19 15:13:37 +0200

    Category buttons syling and making dinamic

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->with('category', 'author')->get()
+        'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all(),
     ]);
 });
 
@@ -32,7 +33,9 @@ Route::get('/posts/{post}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 });
 
