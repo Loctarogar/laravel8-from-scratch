8734c330f0c4d7e0061c6d2e37c6cc99f4f0be8d
Loctarogar <Morilon@protonmail.com>
2023-07-20 12:11:18 +0200

    advanced filtering with eloquent

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,6 +19,12 @@ class Post extends Model
                 ->where('title', 'like', '%' . request('search') . '%')
                 ->orWhere('body', 'like', '%' . request('search') . '%');
         }
+
+        $query->when($filters['category'] ?? false,fn($query, $category) =>
+            $query->whereHas('category', fn($query) =>
+                $query->where('slug', $category)
+            )
+        );
     }
 
     public function getRouteKeyName()
