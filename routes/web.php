3554b0ebb009dd0b533c0835fa5f7869e1b40686
Loctarogar <Morilon@protonmail.com>
2023-07-24 15:36:48 +0200

    register user + password hashing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\PostsController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostsController::class, 'index'])->name('home');
 Route::get('/posts/{post}', [PostsController::class, 'show'])->name('show');
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
