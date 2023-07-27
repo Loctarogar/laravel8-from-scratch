d6d61738a5f1acab84a469283d3f27ea7d782602
Loctarogar <Morilon@protonmail.com>
2023-07-27 14:32:02 +0200

    mailchimp api start

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,21 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('ping', function () {
+//    require_once('/path/to/MailchimpMarketing/vendor/autoload.php');
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us12'
+    ]);
+
+    $response = $mailchimp->lists->getListMembersInfo('0e7e489a98');
+    dd($response);
+//    print_r($response);
+});
+
 Route::get('/', [PostsController::class, 'index'])->name('home');
 
 Route::get('/posts/{post}', [PostsController::class, 'show'])->name('show');
